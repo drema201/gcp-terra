e8f410c2e6449bb6ad7b1a9b0ef5859def8b302d
drema201 <drema201@mail.ru>
2021-02-14 23:56:26 +0300

    added service accout, step 2

--- gcp-fileio/main.tf
+++ gcp-fileio/main.tf
@@ -4,12 +4,14 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
+data "google_compute_default_service_account" "default" {
+}
+
 data "google_compute_image" "image-terra-io" {
   provider = google-beta
   family  = "centos-7"
   project = "centos-cloud"
 }
-
 
 resource "google_compute_instance" "terra-io1" {
   provider = google-beta
@@ -17,10 +19,11 @@ resource "google_compute_instance" "terra-io1" {
   machine_type   = "e2-standard-2"
   zone           = "us-central1-c"
   can_ip_forward = false
-#  service_account {
-#     email = data.google_compute_default_service_account.default.email
-#     scopes = ["cloud-platform"]
-#  }
+  service_account {
+     email = data.google_compute_default_service_account.default.email
+     scopes = ["cloud-platform"]
+     }
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.image-terra-io.self_link
