6223c27df270e7438395a112beba210bcbc1c16b
drema201 <drema201@mail.ru>
2021-02-14 20:52:22 +0300

    some fix

--- gcp-fileio/main.tf
+++ gcp-fileio/main.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "terra-io1" {
 #  }
   boot_disk {
     initialize_params {
-      image = data.google_compute_image.image-terra.self_link
+      image = data.google_compute_image.image-terra-io.self_link
     }
   }
   network_interface {
