cb3781af8172d96cbd904b2ad80db98145e3cc2f
drema201 <drema201@mail.ru>
2021-02-20 18:49:26 +0300

    added yum command

--- orcl19/main.tf
+++ orcl19/main.tf
@@ -15,9 +15,9 @@ data "google_compute_image" "image-terra-ora" {
 
 resource "google_compute_instance" "terra-ora1" {
   provider = google-beta
-  name           = "terra-inst-oar1"
+  name           = "terra-inst-ora1"
   machine_type   = "e2-standard-2"
-  zone           = "us-central1-c"
+  zone           = "us-central1-b"
   can_ip_forward = false
   service_account {
      email = data.google_compute_default_service_account.default.email
@@ -31,9 +31,16 @@ resource "google_compute_instance" "terra-ora1" {
   }
   network_interface {
     network = "default"
+    access_config {
+   //network_tier = "PREMIUM"
+    }
+
   }
 
   metadata_startup_script = <<EOF
+yum install -y wget
+cd /etc/yum.repo.d/
+wget http://yum.oracle.com/public-yum-ol7.repo
 yum install -y oracle-database-preinstall-19c openssl
 EOF
 
