63aac4e79ba067806bada900f9282c4fa37dfdb3
drema201 <drema201@mail.ru>
2021-02-20 19:36:42 +0300

    Saving files before refreshing line endings

--- orcl19/main.tf
+++ orcl19/main.tf
@@ -1,50 +1,50 @@
 provider "google" {
-  project     = "postgretrial"
-  region      = "us-central1"
-  zone        = "us-central1-b"
-}
-
-data "google_compute_default_service_account" "default" {
-}
-
-data "google_compute_image" "image-terra-ora" {
-  provider = google-beta
-  family  = "centos-7"
-  project = "centos-cloud"
-}
-
-resource "google_compute_instance" "terra-ora1" {
-  provider = google-beta
-  name           = "terra-inst-ora1"
-  machine_type   = "e2-standard-2"
-  zone           = "us-central1-b"
-  can_ip_forward = false
-  service_account {
-     email = data.google_compute_default_service_account.default.email
-     scopes = ["cloud-platform"]
-     }
-
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.image-terra-ora.self_link
-    }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-   //network_tier = "PREMIUM"
-    }
-
-  }
-
-  metadata_startup_script = <<EOF
-sleep 10
-yum -y install wget
-sleep 3
-cd /etc/yum.repos.d/
-wget http://yum.oracle.com/public-yum-ol7.repo
-yum -y --nogpgcheck install  oracle-database-preinstall-19c openssl
-EOF
-
-}
-
+  project     = "postgretrial"    
+  region      = "us-central1"    
+  zone        = "us-central1-b"    
+}    
+    
+data "google_compute_default_service_account" "default" {    
+}    
+    
+data "google_compute_image" "image-terra-ora" {    
+  provider = google-beta    
+  family  = "centos-7"    
+  project = "centos-cloud"    
+}    
+    
+resource "google_compute_instance" "terra-ora1" {    
+  provider = google-beta    
+  name           = "terra-inst-ora1"    
+  machine_type   = "e2-standard-2"    
+  zone           = "us-central1-b"    
+  can_ip_forward = false    
+  service_account {    
+     email = data.google_compute_default_service_account.default.email    
+     scopes = ["cloud-platform"]    
+     }    
+    
+  boot_disk {    
+    initialize_params {    
+      image = data.google_compute_image.image-terra-ora.self_link    
+    }    
+  }    
+  network_interface {    
+    network = "default"    
+    access_config {    
+   //network_tier = "PREMIUM"    
+    }    
+    
+  }    
+    
+  metadata_startup_script = <<EOF    
+sleep 10    
+yum -y install wget    
+sleep 3    
+cd /etc/yum.repos.d/    
+wget http://yum.oracle.com/public-yum-ol7.repo    
+yum -y --nogpgcheck install  oracle-database-preinstall-19c openssl    
+EOF    
+    
+}    
+    
