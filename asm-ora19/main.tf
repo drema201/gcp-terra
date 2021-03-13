6598b034d8215b069823f7ff049f9e874cae3c69
drema201 <drema201@mail.ru>
2021-03-13 16:29:14 +0300

    added grid install gen -fix 5

--- asm-ora19/main.tf
+++ asm-ora19/main.tf
@@ -341,59 +341,59 @@ yum --nogpgcheck install kmod-oracleasm
 /usr/sbin/oracleasm listdisks
 
 cat > /tmp/gi_installation.sh << EOL
-$${GI_HOME}/gridSetup.sh -ignorePrereq -waitforcompletion -silent \\\
-    -responseFile $${GI_HOME}/install/response/gridsetup.rsp \\\
-    INVENTORY_LOCATION=/u01/app/oraInventory \\\
-    SELECTED_LANGUAGES=en,en_GB \\\
+$${GI_HOME}/gridSetup.sh -ignorePrereq -waitforcompletion -silent \\\\
+    -responseFile $${GI_HOME}/install/response/gridsetup.rsp \\\\
+    INVENTORY_LOCATION=/u01/app/oraInventory \\\\
+    SELECTED_LANGUAGES=en,en_GB \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle.install.option=HA_CONFIG \\\
+    oracle.install.option=HA_CONFIG \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    ORACLE_BASE=$${GRID_BASE} \\\
-    oracle.install.asm.OSDBA=asmdba \\\
-    oracle.install.asm.OSOPER=asmoper \\\
-    oracle.install.asm.OSASM=asmadmin \\\
+    ORACLE_BASE=$${GRID_BASE} \\\\
+    oracle.install.asm.OSDBA=asmdba \\\\
+    oracle.install.asm.OSOPER=asmoper \\\\
+    oracle.install.asm.OSASM=asmadmin \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle.install.crs.config.ClusterConfiguration=STANDALONE \\\
-    oracle.install.crs.config.configureAsExtendedCluster=false \\\
-    oracle.install.crs.config.clusterName=ol7-rac-c \\\
+    oracle.install.crs.config.ClusterConfiguration=STANDALONE \\\\
+    oracle.install.crs.config.configureAsExtendedCluster=false \\\\
+    oracle.install.crs.config.clusterName=ol7-rac-c \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle_install_crs_ConfigureMgmtDB=false \\\
+    oracle_install_crs_ConfigureMgmtDB=false \\\\
 EOF
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle.install.crs.config.gpnp.configureGNS=false \\\
-    oracle.install.crs.config.autoConfigureClusterNodeVIP=false \\\
-    oracle.install.asm.configureGIMRDataDG=false \\\
-    oracle.install.crs.config.useIPMI=false \\\
-    oracle.install.asm.storageOption=ASM \\\
-    oracle.install.asmOnNAS.configureGIMRDataDG=false \\\
-    oracle.install.asm.SYSASMPassword=welcome1 \\\
-    oracle.install.asm.diskGroup.name=DATA \\\
-    oracle.install.asm.diskGroup.redundancy=EXTERNAL \\\
-    oracle.install.asm.diskGroup.AUSize=4 \\\
+    oracle.install.crs.config.gpnp.configureGNS=false \\\\
+    oracle.install.crs.config.autoConfigureClusterNodeVIP=false \\\\
+    oracle.install.asm.configureGIMRDataDG=false \\\\
+    oracle.install.crs.config.useIPMI=false \\\\
+    oracle.install.asm.storageOption=ASM \\\\
+    oracle.install.asmOnNAS.configureGIMRDataDG=false \\\\
+    oracle.install.asm.SYSASMPassword=welcome1 \\\\
+    oracle.install.asm.diskGroup.name=DATA \\\\
+    oracle.install.asm.diskGroup.redundancy=EXTERNAL \\\\
+    oracle.install.asm.diskGroup.AUSize=4 \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle.install.asm.diskGroup.disksWithFailureGroupNames=/dev/oracleasm/disks/ORCL_DISK1_P1,,/dev/oracleasm/disks/ORCL_DISK2_P1, \\\
-    oracle.install.asm.diskGroup.disks=/dev/oracleasm/disks/ORCL_DISK1_P1,/dev/oracleasm/disks/ORCL_DISK2_P1 \\\
-    oracle.install.asm.diskGroup.diskDiscoveryString=/dev/oracleasm/disks/ORCL_* \\\
+    oracle.install.asm.diskGroup.disksWithFailureGroupNames=/dev/oracleasm/disks/ORCL_DISK1_P1,,/dev/oracleasm/disks/ORCL_DISK2_P1, \\\\
+    oracle.install.asm.diskGroup.disks=/dev/oracleasm/disks/ORCL_DISK1_P1,/dev/oracleasm/disks/ORCL_DISK2_P1 \\\\
+    oracle.install.asm.diskGroup.diskDiscoveryString=/dev/oracleasm/disks/ORCL_* \\\\
 EOL
 
 cat >> /tmp/gi_installation.sh << EOL
-    oracle.install.asm.gimrDG.AUSize=1 \\\
-    oracle.install.asm.monitorPassword=welcome1 \\\
-    oracle.install.crs.configureRHPS=false \\\
-    oracle.install.crs.config.ignoreDownNodes=false \\\
-    oracle.install.config.managementOption=NONE \\\
-    oracle.install.config.omsPort=0 \\\
+    oracle.install.asm.gimrDG.AUSize=1 \\\\
+    oracle.install.asm.monitorPassword=welcome1 \\\\
+    oracle.install.crs.configureRHPS=false \\\\
+    oracle.install.crs.config.ignoreDownNodes=false \\\\
+    oracle.install.config.managementOption=NONE \\\\
+    oracle.install.config.omsPort=0 \\\\
     oracle.install.crs.rootconfig.executeRootScript=false
 EOL
 
