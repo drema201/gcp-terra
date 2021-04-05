bc71ca018936bf767112fa8c326d907322d098e0
drema201 <drema201@mail.ru>
2021-04-05 10:43:40 +0300

    added null provider - check replace

--- tst/main.tf
+++ tst/main.tf
@@ -7,9 +7,15 @@ provider "google" {
 provider "null" {
 }
 
+variable "PREFIX" {
+description ="prefix"
+type=string
+default="10.0.0.0/24"
+}
+
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "echo local"
+    command = "echo replace(${var.PREFIX},'/24','')"
   }
 }
