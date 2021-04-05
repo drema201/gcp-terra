ff722029b4166ddccdeac777fce64090300cdc8d
drema201 <drema201@mail.ru>
2021-04-05 10:30:33 +0300

    added null provider

--- tst/main.tf
+++ tst/main.tf
@@ -2,7 +2,11 @@ provider "google" {
   project     = "postgretrial"    
   region      = "us-central1"    
   zone        = "us-central1-b"    
-}    
+}
+
+provider "null" {
+}
+
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
