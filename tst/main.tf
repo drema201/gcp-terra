09bbb2098d47235c4bb10907e7138fceec979ba0
drema201 <drema201@mail.ru>
2021-04-05 11:11:56 +0300

    added null provider - check cidr func

--- tst/main.tf
+++ tst/main.tf
@@ -16,7 +16,7 @@ default="10.0.0.0/24"
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "echo '${replace(var.PREFIX,'/24','')}'"
+    command = "echo '${replace(var.PREFIX,"/24","")}'"
   }
 
 }
