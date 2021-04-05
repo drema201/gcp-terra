64daad79a47898e6373fe0500ce6c5e0f4d12e87
drema201 <drema201@mail.ru>
2021-04-05 10:51:45 +0300

    added null provider - check replace

--- tst/main.tf
+++ tst/main.tf
@@ -16,6 +16,6 @@ default="10.0.0.0/24"
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    command = "echo ${replace(var.PREFIX,'/24','')}"
+    command = "echo '${replace(var.PREFIX,'/24','')}'"
   }
 }
