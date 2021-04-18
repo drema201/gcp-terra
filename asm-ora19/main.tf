982db930d24d0ac091240eb777d4c9a750414d99
drema201 <drema201@mail.ru>
2021-04-18 20:54:05 +0300

    add custom image creation

--- asm-ora19/main.tf
+++ asm-ora19/main.tf
@@ -272,6 +272,30 @@ data "google_compute_image" "image-terra-ora" {
   }
 }
 
+resource "google_compute_instance" "templ-cenos" {
+  provider = google-beta
+  name           = "terra-inst-asm-01"
+  machine_type   = "e2-standard-1"
+  zone           = "us-central1-b"
+  can_ip_forward = false
+  service_account {
+     email = data.google_compute_default_service_account.default.email
+     scopes = ["cloud-platform"]
+     }
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.image-terra-ora.self_link
+      }
+    }
+}
+
+resource "google_compute_machine_image" "image" {
+  provider        = google-beta
+  name            = "image"
+  source_instance = google_compute_instance.templ-cenos.self_link
+}
+
 resource "google_compute_disk" "disk-b" {
     name    = "disk-b1-data"
     type    = "pd-balanced"
--- tst/main.tf
+++ tst/main.tf
@@ -1,58 +1,23 @@
-provider "google" {
-  project     = "postgretrial"    
-  region      = "us-central1"    
-  zone        = "us-central1-b"    
+resource "google_compute_instance" "templ-cenos" {
+  provider = google-beta
+  name           = "terra-inst-asm-01"
+  machine_type   = "e2-standard-1"
+  zone           = "us-central1-b"
+  can_ip_forward = false
+  service_account {
+     email = data.google_compute_default_service_account.default.email
+     scopes = ["cloud-platform"]
+     }
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.image-terra-ora.self_link
+      }
+    }
 }
 
-provider "null" {
+resource "google_compute_machine_image" "image" {
+  provider        = google-beta
+  name            = "image"
+  source_instance = google_compute_instance.templ-cenos.self_link
 }
-
-variable "PREFIX" {
-description ="prefix"
-type=string
-default="10.0.0.0/24"
-}
-
-
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo '${replace(var.PREFIX,"/24","")}'"
-  }
-
-}
-
-resource "null_resource" "test2" {
-
-  provisioner "local-exec" {
-    command = "echo '${cidrhost(var.PREFIX,0)}'"
-  }
-
-}
-
-resource "google_compute_address" "pubnetwork" {
-  name = "public-ipv4-address"
-  address_type = "EXTERNAL"
-}
-
-resource "google_compute_address" "pubnetwork-2" {
-  name = "public-ipv4-address-2"
-  address_type = "EXTERNAL"
-}
-
-data "google_compute_address" "data-net1" {
-  self_link = google_compute_address.pubnetwork.self_link
-}
-
-data "google_compute_address" "data-net2" {
-  self_link = google_compute_address.pubnetwork-2.self_link
-}
-
-
-output "net-1" {
-  value = data.google_compute_address.data-net1
-}
-
-output "net-2" {
-  value = data.google_compute_address.data-net2
-}
-
