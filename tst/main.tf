a7d968c5e9ffb994761ce7082ece6b96c10962fe
drema201 <drema201@mail.ru>
2021-04-19 00:07:36 +0300

    try public image - remove extra

--- tst/main.tf
+++ tst/main.tf
@@ -14,38 +14,6 @@ data "google_compute_image" "image-terra-ora" {
 
 }
 
-resource "google_compute_instance" "templ-cenos" {
-  provider = google-beta
-  name           = "terra-inst-centos"
-  machine_type   = "e2-standard-2"
-  zone           = "us-central1-b"
-  can_ip_forward = false
-  service_account {
-     email = data.google_compute_default_service_account.default.email
-     scopes = ["cloud-platform"]
-     }
-
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.image-terra-ora.self_link
-      }
-    }
-
-  network_interface {
-    network = "default"
-    access_config {
-   //network_tier = "PREMIUM"
-    }
-  }
-}
-
-resource "google_compute_machine_image" "myimage" {
-  provider        = google-beta
-  name            = "myimage"
-  source_instance = google_compute_instance.templ-cenos.self_link
-
-}
-
 
 
 resource "google_compute_image" "image-base" {
