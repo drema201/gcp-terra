b54e856a79749819911815a7578cd144c5dfcf34
drema201 <drema201@mail.ru>
2021-05-04 15:41:24 +0300

    try to remove pub key type

--- tst/main.tf
+++ tst/main.tf
@@ -23,7 +23,6 @@ resource "google_service_account_key" "orakey" {
   service_account_id = google_service_account.oracle.name
   private_key_type = "TYPE_PKCS12_FILE"
   key_algorithm = "KEY_ALG_RSA_1024"
-  public_key_type = "TYPE_X509_PEM_FILE"
 }
 
 resource "google_compute_image" "image-base1" {
