7841b38af7255e5f37c4b53ae5fe495e882f8d32
drema201 <drema201@mail.ru>
2021-05-07 17:04:03 +0300

    added BigQuery and Storage bucket

--- compos/main.tf
+++ compos/main.tf
@@ -55,3 +55,77 @@ resource "google_composer_environment" "compos-1" {
 
   }
 }
+
+resource "google_bigquery_dataset" "comp_ds" {
+  dataset_id                  = "average_weather"
+  friendly_name               = "Composer_WEATHER"
+  description                 = "This is a test description"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
+resource "google_bigquery_table" "average_weather" {
+  dataset_id = google_bigquery_dataset.comp_ds.dataset_id
+  table_id   = "average_weather"
+
+  deletion_protection ="NO"
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+{
+"name": "location",
+"type": "GEOGRAPHY",
+"mode": "REQUIRED"
+},
+{
+"name": "average_temperature",
+"type": "INTEGER",
+"mode": "REQUIRED"
+},
+{
+"name": "month",
+"type": "STRING",
+"mode": "REQUIRED"
+},
+{
+"name": "inches_of_rain",
+"type": "NUMERIC"
+},
+{
+"name": "is_current",
+"type": "BOOLEAN"
+},
+{
+"name": "latest_measurement",
+"type": "DATE"
+}
+]
+EOF
+
+}
+
+resource "google_storage_bucket" "for-compose" {
+  name          = "compose-gs"
+  location      = "US"
+
+  uniform_bucket_level_access = true
+
+}
