e501a49a9966f1613ed5ff3d1c128b04679d0e2c
drema201 <drema201@mail.ru>
2021-09-13 22:22:30 +0300

    added new dir for pubsub and main.tf

---
+++ pubsub_msg/main.tf
@@ -0,0 +1,8 @@
+provider "google" {
+  project     = "postgretrial"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+data "google_compute_default_service_account" "default" {
+}
