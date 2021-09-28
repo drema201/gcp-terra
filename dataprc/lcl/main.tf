e66edbfec2d42880d6224240dec20c8ad86490da
drema201 <drema201@mail.ru>
2021-09-28 23:09:36 +0300

    add subdir - amend

--- dataprc/lcl/main.tf
+++ dataprc/lcl/main.tf
@@ -4,18 +4,13 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
-resource "google_storage_bucket" "for-dataprc-fs" {
-  name          = "postgretrial-dataproc-fs-bucket"
-  location      = "US"
-  uniform_bucket_level_access = true
-}
 
 
 resource "null_resource" "localcp" {
   provisioner "local-exec" {
-    command = "gsutil cp hello-world.py gs://${google_storage_bucket.for-dataprc-fs.name}/examples/pyspark/hello-world.py"
+    command = "gsutil cp hello-world.py gs://postgretrial-dataproc-fs-bucket/examples/pyspark/hello-world.py"
   }
   provisioner "local-exec" {
-    command = "gsutil cp count_by_subreddit.py gs://${google_storage_bucket.for-dataprc-fs.name}/examples/pyspark/count_by_subreddit.py"
+    command = "gsutil cp count_by_subreddit.py gs://postgretrial-dataproc-fs-bucket/examples/pyspark/count_by_subreddit.py"
   }
 }
