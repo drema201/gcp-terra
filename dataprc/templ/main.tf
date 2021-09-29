c86c159095b2df4b6f85a230c143e61d5c9fe161
drema201 <drema201@mail.ru>
2021-09-29 23:36:44 +0300

    added dataproc workflow template

---
+++ dataprc/templ/main.tf
@@ -0,0 +1,106 @@
+provider "google" {
+  project     = "postgretrial"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+data "google_compute_default_service_account" "default" {
+}
+
+
+resource "google_dataproc_workflow_template" "dataprc-template-ml" {
+  provider = google-beta
+  name = "dataprc-template-ml"
+  location = "us-central1"
+
+  placement {
+    managed_cluster {
+      cluster_name = "dataprc-cluster-ml"
+      config {
+        staging_bucket = google_storage_bucket.for-dataprc.name
+        temp_bucket = google_storage_bucket.for-dataprc-tmp.name
+
+
+        gce_cluster_config {
+          zone = "us-central1-a"
+          tags = ["foo", "bar"]
+          # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+          service_account = data.google_compute_default_service_account.default.email
+          service_account_scopes = [
+            "cloud-platform"
+          ]
+        }
+
+        master_config {
+          num_instances = 1
+          machine_type = "n1-standard-4"
+          disk_config {
+            boot_disk_type = "pd-ssd"
+            boot_disk_size_gb = 15
+          }
+        }
+
+        worker_config {
+          num_instances = 2
+          machine_type = "n1-standard-2"
+          disk_config {
+            boot_disk_size_gb = 10
+            num_local_ssds = 2
+          }
+        }
+
+        # Override or set some custom properties
+        software_config {
+          override_properties = {
+            "dataproc:dataproc.allow.zero.workers" = "true",
+            "spark:spark.pyspark.python" = "/opt/conda/default/bin/python3",
+            "spark-env:PYSPARK_PYTHON"="/opt/conda/default/bin/python3"
+          }
+        }
+
+        # You can define multiple initialization_action blocks
+        initialization_action {
+          script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
+          timeout_sec = 300
+        }
+
+        lifecycle_config {
+          idle_delete_ttl = "1800s"
+        }
+
+      }
+    }
+  }
+  jobs {
+    step_id = "hello"
+    pyspark_job {
+      main_python_file_uri = "gs://dataproc-examples-2f10d78d114f6aaec76462e3c310f31f/src/pyspark/hello-world/hello-world.py"
+      properties = {
+        "spark.logConf" = "true"
+      }
+    }
+  }
+  jobs {
+    step_id = "count-reddit"
+    prerequisite_step_ids = ["hello"]
+    pyspark_job {
+      main_python_file_uri = "gs://postgretrial-dataproc-fs-bucket/examples/pyspark/counts_by_subreddit.py"
+      jar_file_uris = [
+        "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar"
+      ]
+    }
+  }
+
+}
+
+
+
+resource "null_resource" "localcp" {
+  provisioner "local-exec" {
+    command = "gsutil cp ../hello-world.py gs://gs://postgretrial-dataproc-fs-bucket/examples/pyspark/hello-world.py"
+  }
+
+  provisioner "local-exec" {
+    command = "gsutil cp ../counts_by_subreddit.py gs://gs://postgretrial-dataproc-fs-bucket/examples/pyspark/counts_by_subreddit.py"
+  }
+}
