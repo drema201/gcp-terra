32ac62e221df5e641d60dc7dd7dc11c2090f1026
drema201 <drema201@mail.ru>
2021-10-04 07:59:48 +0300

    added dataset and 2 tables

--- big_q/main.tf
+++ big_q/main.tf
@@ -4,3 +4,64 @@ provider "google" {
   zone        = "us-central1-b"
 }
 
+
+resource "google_bigquery_dataset" "ds-test" {
+  dataset_id                  = "ds-test"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.ds-test.dataset_id
+  table_id   = "bar"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
+
+}
+
+resource "google_bigquery_table" "sheet" {
+  dataset_id = google_bigquery_dataset.ds-test.dataset_id
+  table_id   = "sheet"
+
+  external_data_configuration {
+    autodetect    = true
+    source_format = "GOOGLE_SHEETS"
+
+    google_sheets_options {
+      skip_leading_rows = 1
+    }
+
+    source_uris = [
+      "https://docs.google.com/spreadsheets/d/123456789012345",
+    ]
+  }
+}
