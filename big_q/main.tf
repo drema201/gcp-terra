f4acdbe9427005895a8349c2a962508fbf9d7dff
drema201 <drema201@mail.ru>
2021-10-11 00:17:07 +0300

    try add matview

--- big_q/main.tf
+++ big_q/main.tf
@@ -98,6 +98,16 @@ resource "google_bigquery_job" "sqljob_b" {
   }
 }
 
+resource "google_bigquery_job" "sqljob_create1" {
+  job_id     = "job_query_create1"
+
+  query {
+    query = "CREATE MATERIALIZED VIEW postgretrial-id.${google_bigquery_dataset.ds_test.dataset_id}.my_mv_table    OPTIONS (enable_refresh = true, refresh_interval_minutes = 60) AS SELECT count(1), state FROM  usa_1910_2013_1 GROUP BY state"
+    create_disposition = ""
+    write_disposition =""
+
+  }
+}
 
 
 resource "google_bigquery_table" "default" {
