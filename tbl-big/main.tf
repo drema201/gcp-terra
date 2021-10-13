2f59f7bb5a3a89b26cb381f63a9b377aff7f027f
drema201 <drema201@mail.ru>
2021-10-13 09:45:35 +0300

    added fd job, renamings

--- tbl-big/main.tf
+++ tbl-big/main.tf
@@ -4,12 +4,27 @@ provider "google" {
   zone        = "us-central1-b"
 }
 
+resource "google_storage_bucket" "for-btbl" {
+  name          = "postgretrial-bigtbl"
+  location      = "US"
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+  uniform_bucket_level_access = true
+}
+
 resource "google_bigtable_instance" "production-instance" {
-  name = "btbl-instance"
+  name = "bus-instance"
   deletion_protection = false
 
   cluster {
-    cluster_id   = "bgtblclust"
+    cluster_id   = "bus-cluster"
     num_nodes    = 2
     storage_type = "SSD"
   }
@@ -20,16 +35,34 @@ resource "google_bigtable_instance" "production-instance" {
 }
 
 resource "google_bigtable_table" "table_a" {
-  name          = "tbl_a"
+  name          = "bus-data"
   instance_name = google_bigtable_instance.production-instance.name
   split_keys    = ["a", "b", "c"]
   column_family {
-    family = "fml1"
+    family = "cf"
   }
   column_family {
-    family = "fml2"
+    family = "family2"
   }
   lifecycle {
     prevent_destroy = false
   }
+}
+
+resource "google_dataflow_job" "bigtbl_dfj" {
+  name = "bt-test-dataflow-job1"
+  template_gcs_path = "gs://dataflow-templates/latest/GCS_SequenceFile_to_Cloud_Bigtable"
+  temp_gcs_location = "${google_storage_bucket.for-btbl.name}/tmp"
+  //enable_streaming_engine = true
+  parameters = {
+    bigtableProject="postgretrial",
+    bigtableInstanceId="${google_bigtable_instance.production-instance.id}",
+    bigtableTableId="${google_bigtable_table.table_a.name},"
+    sourcePattern="gs://cloud-bigtable-public-datasets/bus-data/*"
+  }
+  transform_name_mapping = {
+    name = "test_job"
+    env = "test"
+  }
+  on_delete = "cancel"
 }
