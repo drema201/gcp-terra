7ba939b1a35a2336452432cff7f0829af59bc924
drema201 <drema201@mail.ru>
2021-10-17 19:39:47 +0300

    change py file

--- tbl-big/bus.py
+++ tbl-big/bus.py
@@ -1,15 +1,17 @@
-/* https://googleapis.dev/python/bigtable/latest/data-api.html
-*/
+##https://googleapis.dev/python/bigtable/latest/data-api.html
+
 from google.cloud import bigtable
 client = bigtable.Client(admin=True)
 
 instance = client.instance("bus-instance")
-//instance.reload()
+# instance.reload()
 
 table = instance.table("bus-data")
 column_families = table.list_column_families()
 print(column_families)
 
 rowKey = "MTA/M86-SBS/1496275200000/NYCT_5824"
-row_data = table.read_row(row_key)
-print(row_data)
+row_data = table.read_row(rowKey)
+print(row_data.cells)
+
+print(row_data.cells[:][:][0])
