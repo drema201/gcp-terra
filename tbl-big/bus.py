10f8f835de135fb5a7cacc4e030dfc0b20081f48
drema201 <drema201@mail.ru>
2021-10-17 19:06:17 +0300

    add py file

---
+++ tbl-big/bus.py
@@ -0,0 +1,15 @@
+/* https://googleapis.dev/python/bigtable/latest/data-api.html
+*/
+from google.cloud import bigtable
+client = bigtable.Client(admin=True)
+
+instance = client.instance("bus-instance")
+//instance.reload()
+
+table = instance.table("bus-data")
+column_families = table.list_column_families()
+print(column_families)
+
+rowKey = "MTA/M86-SBS/1496275200000/NYCT_5824"
+row_data = table.read_row(row_key)
+print(row_data)
