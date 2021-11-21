0a40bad115672326ff5f3e069f6cf21608df8b38
drema201 <drema201@mail.ru>
2021-11-22 01:02:39 +0300

    capitalize

--- bigtbl-crypto/inst/inst.tf
+++ bigtbl-crypto/inst/inst.tf
@@ -39,8 +39,8 @@ resource "google_compute_instance" "default" {
     project_id = "${var.project_id}",
     region = "${var.region}",
     zone = "${var.zone}",
-    bucket_name ="${var.bucket_name}",
-    bucket_folder = "${var.bucket_folder}",
+    bucket_name ="${var.BUCKET_NAME}",
+    bucket_folder = "${var.BUCKET_FOLDER}",
     bigtable_instance_name = "${var.bigtable_instance_name}",
     bigtable_table_name = "${var.bigtable_table_name}",
     bigtable_family_name = "${var.bigtable_family_name}"
