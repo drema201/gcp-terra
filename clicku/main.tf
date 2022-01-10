9491d3f5a9dbb32dd22316ab04224b624f1abbf8
drema201 <drema201@mail.ru>
2022-01-10 22:18:43 +0300

    try one more key

--- clicku/main.tf
+++ clicku/main.tf
@@ -145,7 +145,7 @@ EOF
       host = "terra-inst-click-02"
       type = "ssh"
       user = "daviabidavi"
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = "${file("~/.ssh/terra-davi")}"
     }    //connection_type         = "ssh"
   }
 
@@ -156,7 +156,7 @@ EOF
       host = "terra-inst-click-02"
       type = "ssh"
       user = "daviabidavi"
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = "${file("~/.ssh/terra-davi")}"
     }    //connection_type         = "ssh"
   }
 
