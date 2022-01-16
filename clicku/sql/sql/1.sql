77532a1375c2d6e480e73ba16e690e3092e81719
drema201 <drema201@mail.ru>
2022-01-16 13:58:16 +0300

    new table

--- clicku/sql/sql/1.sql
+++ clicku/sql/sql/1.sql
@@ -3,3 +3,5 @@ USE test;
 CREATE TABLE  IF NOT EXISTS t1 (x String) ENGINE = Memory AS SELECT 1;
 
 CREATE TABLE IF NOT EXISTS all_hits ON CLUSTER gcp_2shards(p Date, i Int32) ENGINE = Distributed(gcp_2shards, default, hits);
+
+CREATE TABLE IF NOT EXISTS mytable ON CLUSTER gcp_2shards(p Date, i Int32. s string) ENGINE = Distributed(gcp_2shards, default, hits);
