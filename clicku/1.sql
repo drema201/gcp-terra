23cd395e928c2f3fe9ab0a105d27199fd2343d24
drema201 <drema201@mail.ru>
2022-01-16 09:42:39 +0300

    try templating

--- clicku/1.sql
+++ clicku/1.sql
@@ -3,4 +3,7 @@ CREATE DATABASE IF NOT EXISTS test;
 
 USE test;
 
-CREATE TABLE t1 (x String) ENGINE = Memory AS SELECT 1;
+CREATE TABLE  IF NOT EXISTS t1 (x String) ENGINE = Memory AS SELECT 1;
+
+CREATE TABLE IF NOT EXISTS all_hits ON CLUSTER gcp_2shards(p Date, i Int32) ENGINE = Distributed(gcp_2shards, default, hits);
+
