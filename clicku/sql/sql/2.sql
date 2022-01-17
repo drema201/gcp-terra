9e4970e17e48698b223ca9bd17e09340efc6e2b6
drema201 <drema201@mail.ru>
2022-01-17 16:53:05 +0300

    added SQLs and .ignore

--- clicku/sql/sql/2.sql
+++ clicku/sql/sql/2.sql
@@ -1,4 +1,25 @@
---clickhouse-client --queries-file 1.sql
-CREATE DATABASE IF NOT EXISTS test;
+CREATE DATABASE  IF NOT EXISTS shardonly;
+USE shardonly;
 
+CREATE TABLE shard_test
+(
+ id Int64,
+ event_time DateTime
+)
+Engine=MergeTree()
+PARTITION BY toYYYYMMDD(event_time)
+ORDER BY id;
 
+CREATE TABLE dist_test
+(
+ id Int64,
+ event_time DateTime
+)
+ENGINE = Distributed('gcp_2shards', 'shardonly', shard_test, rand());
+
+INSERT INTO dist_test (id, event_time) VALUES (1, now());
+
+INSERT INTO dist_test (id, event_time) VALUES (2, now());
+
+INSERT INTO dist_test (id, event_time)
+select number, now() from numbers(100);
