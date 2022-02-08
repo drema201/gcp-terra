7f41ed2967646d888aa0704b2d4f7b31b824f8ee
drema201 <drema201@mail.ru>
2022-02-08 09:07:01 +0300

    1st cluster (VPC native)

--- gke-cli/main.tf
+++ gke-cli/main.tf
@@ -11,3 +11,39 @@ provider "null" {
 data "google_compute_default_service_account" "default" {    
 }
 
+resource "google_compute_subnetwork" "gkesubnet" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.gkenet.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
+
+resource "google_compute_network" "gkenet" {
+  name                    = "gke-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_container_cluster" "vpc_native_gke" {
+  name               = "vpc-native-gke"
+  location           = "us-central1"
+  initial_node_count = 1
+
+  network    = google_compute_network.gkenet.id
+  subnetwork = google_compute_subnetwork.gkesubnet.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "services-range"
+    services_secondary_range_name = google_compute_subnetwork.gkesubnet.secondary_ip_range.1.range_name
+  }
+
+  # other settings...
+}
