2a8ac3bcd6a34c9787513203415cd5f59aa54834
drema201 <drema201@mail.ru>
2022-02-10 09:40:21 +0300

    added service account in one more place

--- gke-cli/main.tf
+++ gke-cli/main.tf
@@ -78,7 +78,12 @@ resource "google_container_cluster" "vpc_native_gke" {
   }
 
   # other settings...
+  service_account = google_service_account.gkecli.email
+  oauth_scopes    = [
+    "https://www.googleapis.com/auth/cloud-platform"
+  ]
 }
+
 
 resource "google_container_node_pool" "vpc_native_gke_nodes" {
   name       = "vpc_native_gke-pool"
