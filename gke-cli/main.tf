002c2d76056609eb09cd527fa7a8c813ccc8c26b
drema201 <drema201@mail.ru>
2022-02-13 23:42:35 +0300

    adjust IP range

--- gke-cli/main.tf
+++ gke-cli/main.tf
@@ -123,12 +123,12 @@ resource "google_compute_subnetwork" "gkesubnet" {
   network       = google_compute_network.gkenet.id
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "192.168.1.0/27"
+    ip_cidr_range = "192.168.1.0/24"
   }
 
   secondary_ip_range {
     range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.64.0/27"
+    ip_cidr_range = "192.168.64.0/24"
   }
 }
 
