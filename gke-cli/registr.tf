6ce5d31da7c5e8646b05742648e8778ed6dfeadf
drema201 <drema201@mail.ru>
2022-02-20 18:34:26 +0300

    added artifact repo

---
+++ gke-cli/registr.tf
@@ -0,0 +1,19 @@
+resource "google_artifact_registry_repository" "click-repo" {
+  provider = google-beta
+
+  location = "us-central1"
+  repository_id = "click-repo"
+  description = "example docker repository for Click"
+  format = "DOCKER"
+}
+
+resource "google_artifact_registry_repository_iam_binding" "binding" {
+  provider = google-beta
+  project = google_artifact_registry_repository.click-repo.project
+  location = google_artifact_registry_repository.click-repo.location
+  repository = google_artifact_registry_repository.click-repo.name
+  role = "roles/viewer"
+  members = [
+    "user:daviabidavi@google.com",
+  ]
+}
